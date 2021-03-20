1bc3ba60530b168c6abc863836bf15b66bd46778
jakubiszon26 <itcppprograming@gmail.com>
2021-03-20 23:04:57 +0100

    animated background and container

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Particles from 'react-particles-js';
+import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles />
+      <div className="dashboard">
+        <h1>test</h1>
+      </div>
     </div>
   );
 }
