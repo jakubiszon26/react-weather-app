3807d6c602134525e3f830f7e853ac448dfe6b3a
jakubiszon26 <itcppprograming@gmail.com>
2021-03-22 20:58:46 +0100

    eslint fix

--- src/components/CityInput.js
+++ src/components/CityInput.js
@@ -2,6 +2,7 @@ import debounce from "lodash.debounce";
 import React, { useCallback } from "react";
 
 export default function CityInput({ setCity }) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const debouncedInput = useCallback(
     debounce((e) => setCity(e.target.value), 500),
     [setCity]
