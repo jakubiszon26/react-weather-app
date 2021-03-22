63182c19ca4edf5aa1b2cc73d937c33acd9dd2fb
jakubiszon26 <itcppprograming@gmail.com>
2021-03-22 21:08:40 +0100

    time change

--- src/components/CityInput.js
+++ src/components/CityInput.js
@@ -4,7 +4,7 @@ import React, { useCallback } from "react";
 export default function CityInput({ setCity }) {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const debouncedInput = useCallback(
-    debounce((e) => setCity(e.target.value), 500),
+    debounce((e) => setCity(e.target.value), 300),
     [setCity]
   );
 
